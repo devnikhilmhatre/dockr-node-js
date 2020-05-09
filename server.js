dd6a88fc98bc3813e2795f95f8572bbbf5a1ca91
nikhil <dev.nikhilmhatre@gmail.com>
2020-05-09 20:57:39 +0530

    docker composer: Incomplete setup

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require('express')
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27018/docker', {useNewUrlParser: true});
 
 const app = express()
 
-const port = process.env.PORT
+const port = process.env.PORT || 3001
+
+var db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('connected..');
+})
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
